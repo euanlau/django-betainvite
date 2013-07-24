b8e1e5a926bc11d2d8ea975ad24496fca444f09e
Euan Lau <euanlau@gmail.com>
2013-07-24 12:56:28 +0800

    Add console log  send invitations command

--- betainvite/management/commands/send_waitlist_invites.py
+++ betainvite/management/commands/send_waitlist_invites.py
@@ -3,17 +3,16 @@ from optparse import make_option
 
 class Command(BaseCommand):
     help = "Send invitations to people on the waiting list"
-    option_list = BaseCommand.option_list + (
-        make_option('-c', '--count',
-                    type="int",
-                    default = 10,
-                    dest="count",
-                    help='number of new invitations'),
-    )
-
     def handle(self, *args, **options):
+        try:
+            count = args[0]
+        except IndexError:
+            print u'usage :', __name__.split('.')[-1], 'number_of_invitations'
+            return
+
         from betainvite.models import WaitingListEntry
-        count = options.get('count')
         entries = WaitingListEntry.objects.filter(invited=False)[:count]
+        print "Sending invitations to %d people" % (entries.count())
         for entry in entries:
+            print "Sending invitation to %s" % (entry.email)
             entry.send_invitation()
